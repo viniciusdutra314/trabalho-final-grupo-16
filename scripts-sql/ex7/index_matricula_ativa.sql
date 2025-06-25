16fa705557e363e5c71c60b61ba112212010aec6
viniciusdutra314 <dutravinisousa2@usp.br>
2025-06-24 22:45:33 -0300

    index indentation

--- scripts-sql/ex7/index_matricula_ativa.sql
+++ scripts-sql/ex7/index_matricula_ativa.sql
@@ -1,1 +1,2 @@
-CREATE INDEX ix_matricula_status_ativa ON matricula (status_matricula) WHERE status_matricula = 'Ativa'
+CREATE INDEX ix_matricula_status_ativa ON matricula (status_matricula) 
+WHERE status_matricula = 'Ativa'
